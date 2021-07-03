6b9d075ac57f880bf3a9dd6bb3bf1fd3262a5a0f
黄聪 <cong.huang02@hand-china.com>
2021-07-04 00:37:31 +0800

    [imp]: 注释添加

--- binary_stream/index.js
+++ binary_stream/index.js
@@ -1,5 +1,9 @@
 // axios
 
+/**
+ * 后端：buffer（流）  文本内容
+ * 前端：文本解析
+ */
 const textBB = () => {
   axios
     .get("http://localhost:3000/textbb", {
@@ -13,15 +17,18 @@ const textBB = () => {
       console.log("string", string);
     });
 };
-
-const textBS = () => {
+/**
+ * 后端: 文件（读流）图片
+ * 前端: ArrayBuffer -> Blob -> createObjectURL -> blob url
+ */
+const textBC = () => {
   axios
     .get("http://localhost:3000/textbc", {
-      //   responseType: "blob",
       responseType: "arraybuffer",
     })
     .then((resp) => {
       const { data } = resp;
+      console.log("data", data);
       const blob = new Blob([data]);
 
       const link = document.createElement("a");
@@ -31,7 +38,10 @@ const textBS = () => {
       link.click();
     });
 };
-
+/**
+ * 后端: 文件（读流）图片
+ * 前端: Blob -> createObjectURL -> blob url
+ */
 const textBSB = () => {
   axios
     .get("http://localhost:3000/textbc", {
@@ -40,6 +50,7 @@ const textBSB = () => {
     })
     .then((resp) => {
       const { data } = resp;
+      console.log("data", data);
       const blob = new Blob([data]);
 
       const link = document.createElement("a");
@@ -49,7 +60,11 @@ const textBSB = () => {
       link.click();
     });
 };
-
+/**
+ * base64使用
+ * 前端 Blog -> FileReader -> dataUrl data:[<mediatype>][;base64],<data>
+ * @returns
+ */
 const getDataUrl = () => {
   return new Promise((resolve, reject) => {
     let blob = new Blob(["Hello, world!"], { type: "text/plain" });
@@ -61,20 +76,26 @@ const getDataUrl = () => {
     reader.onerror = reject;
   });
 };
-
+/**
+ * dataUrl
+ */
 const initDownFile2 = async () => {
   const downFile2Link = document.querySelector("#downFile2");
   downFile2Link.download = "测试2.txt";
   const dataurl = await getDataUrl();
   downFile2Link.href = dataurl;
 };
-
+/**
+ * Blob -> createObjectURL -> blob url
+ */
 const initDownFile = () => {
   const downFileLink = document.querySelector("#downFile");
   downFileLink.href = URL.createObjectURL(new Blob(["文件下载"]));
   downFileLink.download = "测试.txt";
 };
-
+/**
+ * 前端: img -> canvasImg -> canvas2Blob ->createObjectURL -> blob url
+ */
 const handleImgAndDownload = async () => {
   const img = document.querySelector("#img1");
 
--- index.js
+++ index.js
@@ -9,17 +9,20 @@ const fs = require("fs");
 const app = new Koa();
 
 const router = new Router();
-// 文本的二进制
+// 文本的buffer
 router.get("/textbb", (ctx, next) => {
   ctx.body = Buffer.from("这是一段文本流");
 });
 
-// 文本的二进制
+// 文件的流
 router.get("/textbc", (ctx, next) => {
   const readStream = fs.createReadStream("./test.png");
   ctx.body = readStream;
 });
 
+/**
+ * 固定名称： a链接可下载
+ */
 router.get("/textf", (ctx, next) => {
   const filename = "嘉然.png";
 
@@ -31,7 +34,9 @@ router.get("/textf", (ctx, next) => {
   const readStream = fs.createReadStream("./test.png");
   ctx.body = readStream;
 });
-
+/**
+ * 路径固定名称：a链接可下载
+ */
 router.get("/diana.png", (ctx, next) => {
   const readStream = fs.createReadStream("./test.png");
   ctx.body = readStream;
